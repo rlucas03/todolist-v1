40d1f9127647e035bf87a2b1b7ee687f20e91910
Ryan Lucas <daytripper@pounces-imac-2.home>
2021-02-19 20:42:25 +0000

    complete to do list now updating

--- app.js
+++ app.js
@@ -5,48 +5,54 @@ const bodyParser = require('body-parser');
 // create a new instance of express
 const app = express();
 
+let items = ['Buy food', 'Cook Food', 'Eat Food'];
+// items increases ++ by one and newitem gets pushed onto the end of the array
+// this whole thing is a fundamental part of templating
+
 // this line tells our code which is generated 
 // using express to use ejs as its view engine
 // the views directory is where the view engine by default will go and look for the files we are trying to render
 app.set('view engine', 'ejs');
 
-// when user tries to access home route we just send hello atm
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+//  when we first load up our home page we go through this get route
+// and we render our list.ejs passing in 2 variables kindofday and newlistitems which is set to equal the items array
 app.get('/', function(req, res) {
 
 let today = new Date();
 let currentDay = today.getDay();
-let day = '';
 
 // we are using a new method: res.render and 
 // this uses the view engine that we set 
 //up above to render a particular page, e.g list and that is a .ejs page 
+// going to use our options object to render our date
 
-switch(currentDay) {
-	case 0:
-		day = 'Sunday';
-		break;
-		case 1:
-		day = 'Monday';
-		break;
-	case 2:
-		day = 'Tuesday';
-		break;
-	case 3:
-		day = 'Wednesday';
-		break;
-	case 4:
-		day = 'Thursday';
-		break;
-	case 5:
-		day = 'Friday';
-		break;
-	case 6:
-		day = 'Saturday';
-		break;
-	default:
-	console.log(`Error: current day is equal to ${currentDay}`);
-}
+let options = {
+	weekday: 'long',
+	day: 'numeric',
+	month: 'long'
+};
 
+// this formatted day gets passed into day in res.render below
+let day = today.toLocaleDateString("en-US", options);
+
+// which gets sent over to our list.ejs as a variable called kindOfDay
+// when we pass item over to list view we pass over our entire array
+	res.render('list', {kindOfDay: day, newListItems: items });
+});
+
+
+// handle post requests to the home / route
+app.post('/', function(req, res) {
+	let item = req.body.newItem;
+	items.push(item);
+	console.log(item);
+	res.redirect('/');
+	// redirect to get back into the .get homeroute code and update item because wehad scope issue with item var here
+
+});
 
 	// key value pair in obj
 	// this basically says render a file called list
@@ -58,10 +64,7 @@ extension .ejs and then into that list file we are passing in a single variable
 that has the name of kindOfDay and the valiue we are giving it is the value of
 our variable day. 
  */
-	res.render('list', {kindOfDay: day});
 
-
-});
 
 app.listen(3000, function() {
 	console.log('Server started on port 3000');
